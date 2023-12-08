b65ee5a4dd917ecd8ea8773f4e6ac9677e56128f
EbenBoakye <kaypee340@gmail.com>
2023-12-08 23:18:03 +0000

    fixed Hero issue

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,10 +4,10 @@ const Hero = () => {
     return (
         <div className='text-white'>
             <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
-                <p className='font-bold p-2'>Tenant Shield® IS A WED TOOL DESINGED FOR TENSNTS FACING EVICTION DUE TO RENT ARREAS WHO ARE BEING ASKED TO APPEAR IN COURT</p>
+                <p className='font-bold p-2'>Tenant Shield® IS A WEB TOOL DESINGED FOR TENSNTS FACING EVICTION DUE TO RENT ARREAS WHO ARE BEING ASKED TO APPEAR IN COURT</p>
                 <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold'>THIS TOOL IS SUITABLE ALL TENANTS, WHETHER ELIGIBLE FOR LEGAL AID OR OTHERWISE</h1>
                 <div>
-                   <P>Your presence </P>
+                   <p> Your presence </p>
                </div>
             </div>
         </div>
