33950c4001d0bcdca3c4a35283188d6769b151db
EbenBoakye <kaypee340@gmail.com>
2023-12-08 19:58:44 +0000

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto text-white'>
-            <h1 className='w-full text-3xl font bold text-white'>Tenant Shield</h1>
+        <div className='flex justify-between items-center h-24 max-w-[1240px] px-4 mx-auto text-white'>
+            <h1 className='w-full text-4xl font bold text-white'>Tenant Shield®</h1>
             <ul className='flex'>
             <li className='p-4'>Home</li>
             <li className='p-4'>Eligiblity</li>
