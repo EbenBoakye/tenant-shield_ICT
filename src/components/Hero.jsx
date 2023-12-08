aaec8d2c1b76f95d4ad8e943f85f18e533f26889
EbenBoakye <kaypee340@gmail.com>
2023-12-08 23:26:41 +0000

    Changed font size and colour for Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,8 +4,9 @@ const Hero = () => {
     return (
         <div className='text-white'>
             <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
-                <p className='font-bold p-2'>Tenant Shield® IS A WEB TOOL DESINGED FOR TENSNTS FACING EVICTION DUE TO RENT ARREAS WHO ARE BEING ASKED TO APPEAR IN COURT</p>
-                <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold'>THIS TOOL IS SUITABLE ALL TENANTS, WHETHER ELIGIBLE FOR LEGAL AID OR OTHERWISE</h1>
+                <p className='font-bold p-2'>Tenant Shield® IS A WEB TOOL DESINGED FOR TENSNTS FACING EVICTION DUE TO RENT ARREAS WHO ARE BEING ASKED TO APPEAR IN COURT.</p>
+                <p className='font-bold p-3'>THIS TOOL IS SUITABLE ALL TENANTS, WHETHER ELIGIBLE FOR LEGAL AID OR OTHERWISE.</p>
+                <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold text-red-500'>IT IS IN YOUR BEST INTEREST TO SHOW UP FOR YOUR COURT HEARING </h1>
                 <div>
                    <p> Your presence </p>
                </div>
