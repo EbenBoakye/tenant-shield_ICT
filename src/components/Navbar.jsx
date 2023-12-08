7e49022861b46a141be8955d46abcca37f9e1fc3
EbenBoakye <kaypee340@gmail.com>
2023-12-08 19:49:04 +0000

    icon changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <div className='flex justify-between text-white'>
+        <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto text-white'>
             <h1 className='w-full text-3xl font bold text-white'>Tenant Shield</h1>
             <ul className='flex'>
             <li className='p-4'>Home</li>
-            <li className='p-4'>Check Eligiblity</li>
+            <li className='p-4'>Eligiblity</li>
             <li className='p-4'>Resources</li>
             <li className='p-4'>About</li>
 
