0b82773d20921d08ce89d1d24ef8c943d8a91522
EbenBoakye <117763566+EbenBoakye@users.noreply.github.com>
2023-12-09 23:11:37 +0000

    added form with css

--- src/App.js
+++ src/App.js
@@ -3,19 +3,21 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import TextAni from './components/TextAni';
-
-
-
-
+import Form from './pages/Form';
 
 function App() {
   return (
-    <div>
-    < Navbar /> 
-    <Hero /> 
-    <TextAni />
-    </div>
-  )
+    
+      <div>
+        <Navbar />
+        {/* <Form /> */}
+        <Hero />
+        <TextAni />
+        
+      </div>
+    
+  );
 }
 
 export default App;
+
