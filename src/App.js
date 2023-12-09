6c46d424692d483f2dec5ea909075b29901a7ea8
EbenBoakye <117763566+EbenBoakye@users.noreply.github.com>
2023-12-09 13:39:00 +0000

    Added text animation

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import TextAni from './components/TextAni';
 
 
 
@@ -11,7 +12,8 @@ function App() {
   return (
     <div>
     < Navbar /> 
-    <Hero />
+    <Hero /> 
+    <TextAni />
     </div>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const Hero = () => {
+    
     return (
         <div className='text-white'>
             <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
@@ -8,7 +9,9 @@ const Hero = () => {
                 <p className='font-bold p-3'>THIS TOOL IS SUITABLE ALL TENANTS, WHETHER ELIGIBLE FOR LEGAL AID OR OTHERWISE.</p>
                 <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold text-red-500'>IT IS IN YOUR BEST INTEREST TO SHOW UP FOR YOUR COURT HEARING </h1>
                 <div>
-                   <p> Your presence </p>
+                   {/* <p className='md:text-5xl sm:text-4xl text xl font-bold'> Your presence </p>
+                   <p className='md:text-5xl sm:text-4xl text xl font-bold'> is your best defense </p>       */}
+
                </div>
             </div>
         </div>
