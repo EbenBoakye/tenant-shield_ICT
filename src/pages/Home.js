d52b55e47bfad92a40d6c4cf0fa06ed311943346
wailwinphone <146435563+wailwinphone@users.noreply.github.com>
2023-12-10 11:16:39 +0000

    Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function Home() {
   return (
     <div>
-      <Link to="/form"> Let's get started </Link>
+      <Link to="/form" className='bg-[#5C8374] hover:bg-[#9cc0b2] text-white font-bold py-2 px-4 rounded-full mt-10'> Get Started </Link>
     </div>
   );
 }
