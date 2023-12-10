bf8ffabc43ccbbcc2842b54682ad93c38693d975
wailwinphone <146435563+wailwinphone@users.noreply.github.com>
2023-12-10 10:22:11 +0000

    App.js

--- src/App.js
+++ src/App.js
@@ -4,14 +4,19 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import TextAni from './components/TextAni';
 import Form from './pages/Form';
+//This is the method to call for the form page
+  import Form from './pages/Form';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
+//
 function App() {
   return (
     
       <div>
         <Navbar />
         {/* <Form /> */}
-    //  <Router>
+    // This is the page to route to the form page <Router>
                     <Routes>
                     <Route path="/" element={<Home />}/>
                     <Route path="/form" element={<Form />}/>
