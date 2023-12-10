e4cbd606e13fddd0ef4b4a0c11da4d9b086979e1
wailwinphone <146435563+wailwinphone@users.noreply.github.com>
2023-12-10 10:19:05 +0000

    Create Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Home() {
+  return (
+    <div>
+      <Link to="/form"> Let's get started </Link>
+    </div>
+  );
+}
+
+export default Home;
